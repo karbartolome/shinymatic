cfa8f40190f7e49641077d6d103e68b2e624f9c5
karbartolome <karinabartolome@hotmail.com>
2022-06-17 01:09:31 -0300

    :tada: Inicial commit

---
+++ R/slider_numeric.R
@@ -0,0 +1,52 @@
+#' numeric_slider
+#'
+#' @param .df Dataframe with N variables (one or more should be numeric)
+#'
+#' @return SliderInputs for all the numeric variables
+#' @import shiny
+#' @export
+#'
+#' @examples
+#' library(shiny)
+#' library(shinydfinputs)
+#' df <- data.frame(
+#'   var_num_1 = c(1,2,3),
+#'   var_num_2 = c(6,5,4),
+#'   var_cat_1 = c('a','b','c'))
+#' 
+#' ui <- shiny::fluidPage(
+#'   shinydfinputs::slider_numeric(.df=df),
+#'   shiny::textOutput(outputId = 'values')
+#' )
+#' 
+#' server <- function(input, output) {
+#'   output$values <- reactive({
+#'     vars_num <- names(df[,unlist(lapply(df, is.numeric))])
+#'     paste0(sapply(vars_num, 
+#'                   FUN=function(i) paste(i,"=", input[[i]])), 
+#'            collapse = ', ')
+#'   })
+#' }
+#' 
+#' shiny::shinyApp(ui = ui, server = server)
+slider_numeric <- function(.df) {
+  vars_num <- names(.df[,unlist(lapply(.df, is.numeric))])
+  
+  shiny::tagList(shiny::fluidRow(
+    shiny::column(
+      width = 12,
+      lapply(
+        X = 1:length(vars_num),
+        FUN = function(var) {
+          shiny::sliderInput(
+            inputId = vars_num[var],
+            label = vars_num[var],
+            min = min(.df[, var]),
+            max = max(.df[, var]),
+            step = 1,
+            value = .df[1, var]
+          )
+        }
+      )
+    )))
+}
---
+++ README.md
@@ -0,0 +1,45 @@
+
+# shinydfinputs
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of shinydfinputs is to automatically generate shiny inputs based on a dataframe. 
+
+## Installation
+
+You can install the development version of shinydfinputs from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("karbartolome/shinydfinputs")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(shiny)
+library(shinydfinputs)
+df <- data.frame(
+ var_num_1 = c(1,2,3),
+ var_num_2 = c(6,5,4),
+ var_cat_1 = c('a','b','c'))
+
+ui <- shiny::fluidPage(
+ shinydfinputs::slider_numeric(.df=df),
+ shiny::textOutput(outputId = 'values')
+)
+
+server <- function(input, output) {
+ output$values <- reactive({
+   vars_num <- names(df[,unlist(lapply(df, is.numeric))])
+   paste0(sapply(vars_num, 
+                 FUN=function(i) paste(i,"=", input[[i]])), 
+          collapse = ', ')
+ })
+}
+
+shiny::shinyApp(ui = ui, server = server)
+```
